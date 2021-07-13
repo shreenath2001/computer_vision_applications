85cc6b61cc13f4679a4f20d6632d7d983dc67f31
Shreenath Bharadwaj <bharadwajshreenath@gmail.com>
2021-07-13 07:13:12 +0530

    edited handtracking

--- Hand_Tracking/handTracking.py
+++ Hand_Tracking/handTracking.py
@@ -29,11 +29,26 @@ class HandDetector():
 
                     h,w,c = img.shape
                     cx, cy = int(lm.x * w), int(lm.y * h)
-                    cv2.circle(img, (cx, cy), 15, (255, 100, 0), cv2.FILLED)
+                    cv2.circle(img, (cx, cy), 5, (255, 100, 0), cv2.FILLED)
 
                 self.mpDraw.draw_landmarks(img, hand_lms, self.mpHands.HAND_CONNECTIONS)
 
         return img
+
+    def findPosition(self, img, handNo = 0, draw = False):
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                # print(id, cx, cy)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+        return lmList
 
 
 def main():
