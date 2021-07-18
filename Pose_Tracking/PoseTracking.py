8e9b6c7167782c588b1767e9a8f3b9456a0758d0
Shreenath Bharadwaj <bharadwajshreenath@gmail.com>
2021-07-18 08:21:11 +0530

    edited pose tracking project

--- Pose_Tracking/PoseTracking.py
+++ Pose_Tracking/PoseTracking.py
@@ -1,6 +1,7 @@
 import mediapipe as mp 
 import cv2
 import time
+import math
 
 class PoseDetector():
 
@@ -10,18 +11,57 @@ class PoseDetector():
         self.pose = self.mpPose.Pose()
         self.mpDraw = mp.solutions.drawing_utils 
 
-    def detectPose(self, img):
+    def detectPose(self, img, draw = True):
 
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = self.pose.process(imgRGB)
+        self.results = self.pose.process(imgRGB)
 
-        if results.pose_landmarks:
-            for id,lm in enumerate(results.pose_landmarks.landmark):
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+
+        return img
+
+    def findPosition(self, img, draw = True):
+
+        self.lmList = []
+
+        if self.results.pose_landmarks:
+            for id,lm in enumerate(self.results.pose_landmarks.landmark):
                 h,w,c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                cv2.circle(img, (cx, cy), 15, (255, 100, 0), cv2.FILLED)
-            self.mpDraw.draw_landmarks(img, results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
-        return img
+                self.lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+
+        return self.lmList
+
+    def findAngle(self, img, p1, p2, p3, draw = True):
+
+        # Get the landmarks
+        _, x1, y1 = self.lmList[p1]
+        _, x2, y2 = self.lmList[p2]
+        _, x3, y3 = self.lmList[p3]
+
+        #cal the angle
+        angle = math.degrees(math.atan2(y3-y2, x3-x2)-math.atan2(y1-y2, x1-x2))
+        if angle<0:
+            angle+=360
+        #print(angle)
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
+            cv2.line(img, (x3, y3), (x2, y2), (255, 255, 255), 3)
+            cv2.circle(img, (x1, y1), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x1, y1), 15 ,(0, 0, 255), 2)
+            cv2.circle(img, (x2, y2), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15 ,(0, 0, 255), 2)
+            cv2.circle(img, (x3, y3), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x3, y3), 15 ,(0, 0, 255), 2)
+            #cv2.putText(img, str(int(angle)), (x2 - 50, y2 + 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+
+        return angle
+
 def main():
 
     pTime = 0
