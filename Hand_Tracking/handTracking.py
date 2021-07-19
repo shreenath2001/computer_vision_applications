5c6c1ec36fbe49b4cf5b35d9cc7a8f001139913c
Shreenath Bharadwaj <bharadwajshreenath@gmail.com>
2021-07-20 01:06:23 +0530

    completed handTracking

--- Hand_Tracking/handTracking.py
+++ Hand_Tracking/handTracking.py
@@ -15,6 +15,7 @@ class HandDetector():
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
+        self.tipPos = [4, 8, 12, 16, 20]
 
     def detectHands(self, img):
 
@@ -30,7 +31,7 @@ class HandDetector():
         return img
 
     def findPosition(self, img, handNo = 0, draw = False):
-        lmList = []
+        self.lmList = []
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
             for id, lm in enumerate(myHand.landmark):
@@ -38,11 +39,32 @@ class HandDetector():
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 # print(id, cx, cy)
-                lmList.append([id, cx, cy])
+                self.lmList.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
 
-        return lmList
+        return self.lmList
+
+    def fingersUp(self):
+        fingers = []
+        rigthHand = (self.lmList[5][1]>self.lmList[17][1])
+        if(rigthHand):
+            if self.lmList[self.tipPos[0]][1] > self.lmList[self.tipPos[0]-1][1]: # right thumb
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        else:
+            if self.lmList[self.tipPos[0]][1] < self.lmList[self.tipPos[0]-1][1]: # left thumb
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        for i in range(1, 5):
+            if self.lmList[self.tipPos[i]][2] < self.lmList[self.tipPos[i]-2][2]: # 4 fingers
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        return fingers
 
 
 def main():
