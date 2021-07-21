eb1a8e43b23f368b3e3afa6f160c1702539ae561
Shreenath Bharadwaj <bharadwajshreenath@gmail.com>
2021-07-21 19:41:09 +0530

    updated htm

--- Hand_Tracking/handTracking.py
+++ Hand_Tracking/handTracking.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
+import math
 
 from mediapipe.python.solutions import hands
 
@@ -45,6 +46,22 @@ class HandDetector():
 
         return self.lmList
 
+    def bbox(self, img, handNo = 0, draw = True):
+        xList, yList = [], []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                xList.append(cx)
+                yList.append(cy)
+        xmin, xmax = min(xList), max(xList)
+        ymin, ymax = min(yList), max(yList)
+        bbox = xmin, ymin, xmax, ymax
+        if draw:
+            cv2.rectangle(img, (xmin-20, ymin-20), (xmax+20, ymax+20), (255, 0, 255), 2)
+        return bbox
+
     def fingersUp(self):
         fingers = []
         rigthHand = (self.lmList[5][1]>self.lmList[17][1])
@@ -66,6 +83,19 @@ class HandDetector():
 
         return fingers
 
+    def findDistance(self, p1, p2, img, draw = True, r = 15, t = 3):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+            length = math.hypot(x2-x1, y2-y1)
+
+        return length, img, [x1, y1, x2, y2, cx, cy]
 
 def main():
 
